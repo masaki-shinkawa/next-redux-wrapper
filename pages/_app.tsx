d520cc93950a3fb70a203db368b70dc598fa0c1a
Masaki-Shinkawa <masaki.shinkawa@adglobe.co.jp>
2022-10-27 20:16:13 +0900

    feat: next-redux-wrapperを使用してredux環境の構築

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { wrapper } from "../store/store";
+import { FC } from "react";
+import { Provider } from "react-redux";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App: FC<AppProps> = ({ Component, ...rest }) => {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  return (
+    <Provider store={store}>
+      <Component {...props.pageProps} />
+    </Provider>
+  );
+};
+
+export default App;
